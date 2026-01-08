c9543c48bc58cc46f4fdd12cbcfe1c3a8cb28af0
ABIOVE <febiwulandiniii92@gmail.com>
2026-01-08 07:48:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
-# Load model pipeline (TF-IDF + Model)
-model = joblib.load("pipeline.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "pipeline.pkl")
+model = joblib.load(model_path)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,15 +16,10 @@ def index():
 
     if request.method == "POST":
         text_input = request.form.get("text", "")
-
         if text_input.strip():
-            # Prediksi label
             pred = model.predict([text_input])[0]
-
-            # Confidence (karena pakai MultinomialNB)
             prob = model.predict_proba([text_input]).max()
             confidence = round(prob * 100, 2)
-
             prediction = "valid" if pred == 1 else "hoax"
 
     return render_template(
@@ -31,11 +28,6 @@ def index():
         confidence=confidence,
         text=text_input
     )
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
